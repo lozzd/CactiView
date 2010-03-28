11cdfc5e4fa9bc81fd21c02a3302b6fa61bd1885
Laurie Denness <laurie@denness.net>
2010-03-28 17:54:56 +0000

    Add support for Ganglia graphs in the rotation.

--- config.php
+++ config.php
@@ -16,21 +16,33 @@
 # Time (in milliseconds) before the graphs will rotate automatically.
 $timeout = 20000;
 
-# Path to cacti (on your webserver, including the trailing slash) e.g. http://cactihost/cacti/
-$cactipath = "http://host/cacti/";
+# Path to Cacti (on your webserver, including the trailing slash) e.g. http://cactihost/cacti/
+$cactipath = "https://admintools.last.fm/cacti/";
+# Path to Ganglia (on your webserver, including the trailing slash) e.g. http://gangliahost/ganglia/
+$gangliapath = "https://admintools.last.fm/ganglia/";
 
 
 # Graph definitions
 #
 # Alter the lines below to take the graphs you wish to rotate. 
-# For example, if the Cacti URL http://host/cacti/graph.php?action=view&local_graph_id=558&rra_id=all
-# then your "cactiid" is 558. Then enter a title of your choosing to be display on the graph. 
-#
 # You can define as many graphs as you wish. 
 
+# CACTI: 
+# For example, if the Cacti URL http://host/cacti/graph.php?action=view&local_graph_id=558&rra_id=all
+# then your "cactiid" is 558. Then enter a title of your choosing to be display on the graph. e.g.
+# array("source" => "cacti", "cactiid" => "558" , "title" => "Graph 3" ),
+
+# GANGLIA:
+# Choose a cluster, or a cluster and a host and enter the variables e.g. 
+# array("source" => "ganglia", "cluster" => "Web Servers" , "title" => "Web Servers" ),
+# or for a specific host in that cluster, exactly as it appears in Ganglia (including FQDN, if applicable)
+# array("source" => "ganglia", "cluster" => "Web Servers", "host" => "www1.example.com", "title" => "www1" ),
+
+
 $graphs = array (
-array("cactiid" => 1 , "title" => "Graph 1" ),
-array("cactiid" => 2 , "title" => "Graph 2" ),
+array("source" => "ganglia", "cluster" => "Web Servers" , "title" => "Graph 1" ),
+array("source" => "ganglia", "cluster" => "Web Servers", "host" => "www1" , "title" => "Graph 2" ),
+array("source" => "cacti", "cactiid" => "558" , "title" => "Graph 3" ),
 );
 
 
--- graphview.php
+++ graphview.php
@@ -7,8 +7,23 @@ include("config.php");
 $id = $_GET['id'];
 
 # Set up some variables 
-# The Cacti ID
-$cactigraph = $graphs[$id]['cactiid'];
+
+if ($graphs[$id]['source'] == "cacti") {
+	# The Cacti ID
+	$cactigraph = $graphs[$id]['cactiid'];
+}
+
+if ($graphs[$id]['source'] == "ganglia") {
+	# The Ganglia Cluster ID
+	# A bit of magic here, if there is a host defined, stick the host in the cluster variable too.
+	if(isset($graphs[$id]['host'])) {
+		$gangliacluster = $graphs[$id]['cluster'] . "&h=" . $graphs[$id]['host'];
+	} else {
+		$gangliacluster = $graphs[$id]['cluster']; 
+	}
+
+}
+
 # The title of the graph
 $title = $graphs[$id]["title"];
 
@@ -18,6 +33,29 @@ if ($id < (count($graphs) -1)) {
 } else {
 	$nexttitle = $graphs[0]["title"];
 }
+
+if ($graphs[$id]['source'] == "cacti") {
+	# Construct the URLs for Cacti
+	$endtime = time(); 
+	$starttime = time() - 43200;
+
+	$twelvehour = "{$cactipath}graph_image.php?action=zoom&local_graph_id={$cactigraph}&rra_id=0&graph_height=970&graph_width=1085&graph_nolegend=1&graph_end={$endtime}&graph_start={$starttime}&view_type=tree&notitle=1"; 
+	$week = "{$cactipath}graph_image.php?action=view&local_graph_id={$cactigraph}&rra_id=2&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1";
+	$month = "{$cactipath}graph_image.php?action=view&local_graph_id={$cactigraph}&rra_id=3&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1";
+	$year = "{$cactipath}graph_image.php?action=view&local_graph_id={$cactigraph}&rra_id=4&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1";
+}
+
+
+if ($graphs[$id]['source'] == "ganglia") {
+        # Construct the URLs for Ganglia
+
+	$twelvehour = "{$gangliapath}graph.php?g=load_report&z=large&c={$gangliacluster}&m=load_one&r=hour&s=descending&hc=4&mc=2&width=1050&height=950";
+	$week = "{$gangliapath}graph.php?g=cpu_report&z=medium&c={$gangliacluster}&m=load_one&r=hour&s=descending&hc=4&mc=2&width=400&height=220";
+	$month = "{$gangliapath}graph.php?g=network_report&z=medium&c={$gangliacluster}&m=load_one&r=hour&s=descending&hc=4&mc=2&width=400&height=220";
+	$year = "{$gangliapath}graph.php?g=mem_report&z=medium&c={$gangliacluster}&m=load_one&r=hour&s=descending&hc=4&mc=2&width=400&height=220";
+
+}
+
 
 ?>
 
@@ -37,10 +75,10 @@ body {
 
 <table>
 <tr>
-	<td><img src="<?php echo $cactipath ?>graph_image.php?action=zoom&local_graph_id=<?php echo $cactigraph ?>&rra_id=0&graph_height=970&graph_width=1085&graph_nolegend=1&graph_end=<?php echo time(); ?>&graph_start=<?php echo time() - 43200; ?>&view_type=tree&notitle=1"></td>
-	<td valign="top"><img src="<?php echo $cactipath ?>graph_image.php?action=view&local_graph_id=<?php echo $cactigraph ?>&rra_id=2&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1">
-	<img src="<?php echo $cactipath ?>graph_image.php?action=view&local_graph_id=<?php echo $cactigraph ?>&rra_id=3&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1">
-	<img src="<?php echo $cactipath ?>graph_image.php?action=view&local_graph_id=<?php echo $cactigraph ?>&rra_id=4&graph_height=255&graph_width=400&graph_nolegend=1&notitle=1">
+	<td><img src="<?php echo $twelvehour; ?>"></td>
+	<td valign="top"><img src="<?php echo $week ?>">
+	<img src="<?php echo $month ?>">
+	<img src="<?php echo $year ?>">
 
          <div style="margin-top: 10px; font-size: 48px; text-align: center;"><?php echo date('j/n/y G:i T'); ?></div>
          <div style="font-size: 40px; text-align: center;"><?php echo date_format(date_create('now', timezone_open('EST')), 'j/n/y G:i T'); ?></div>
